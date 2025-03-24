07580645592c616d0579014b9bc18e44b4f424f2
Mx444 <mybbtester@gmail.com>
2025-03-24 11:09:59 +0000

    first commit

---
+++ src/src/main/java/game/HangmanLogic.java
@@ -0,0 +1,223 @@
+package game;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class HangmanLogic {
+    private String[] parole = { "ciao", "computer" };
+    private String parolaDaIndovinare = parole[(int) (Math.random() * parole.length)];
+    private char[] lettereIndovinate;
+    private StringBuilder lettereErrate;
+    private int tentativiRimasti;
+    private boolean partitaFinita;
+
+    // #region Costruttore
+    public HangmanLogic() {
+        caricaParoleDaFile();
+
+        Random random = new Random();
+        this.parolaDaIndovinare = parole[random.nextInt(parole.length)];
+
+        this.lettereIndovinate = new char[parolaDaIndovinare.length()];
+        for (int i = 0; i < lettereIndovinate.length; i++)
+            lettereIndovinate[i] = '_';
+
+        this.lettereErrate = new StringBuilder();
+        this.tentativiRimasti = 6;
+        this.partitaFinita = false;
+    }
+
+    // #endregion
+
+    // #region Logica
+    public boolean indovinaLettera(char lettera) {
+
+        if (!Character.isLetter(lettera)) {
+            System.out.println("Inserisci una lettera valida");
+            return false;
+        }
+        if (partitaFinita)
+            return false;
+
+        lettera = Character.toLowerCase(lettera);
+        if (lettereErrate.indexOf(String.valueOf(lettera)) >= 0 || contieneLettera(lettereIndovinate, lettera)) {
+            System.out.println("Lettera già inserita");
+            return false;
+        }
+
+        boolean letteraTrovata = false;
+        for (int i = 0; i < parolaDaIndovinare.length(); i++) {
+            if (parolaDaIndovinare.charAt(i) == lettera) {
+                lettereIndovinate[i] = lettera;
+                letteraTrovata = true;
+            }
+        }
+
+        if (!letteraTrovata) {
+            lettereErrate.append(lettera);
+            tentativiRimasti--;
+        }
+
+        if (tentativiRimasti == 0)
+            partitaFinita = true;
+
+        if (!new String(lettereIndovinate).contains("_"))
+            partitaFinita = true;
+
+        return letteraTrovata;
+
+    }
+
+    // #endregion
+
+    // #region Utils
+
+    private void caricaParoleDaFile() {
+        List<String> listaParole = new ArrayList<>();
+
+        try {
+            InputStream is = getClass().getResourceAsStream("/resources/words.txt");
+
+            if (is == null) {
+                try (BufferedReader reader = new BufferedReader(new FileReader(
+                        "src/src/main/java/resources/words.txt"))) {
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        if (!line.trim().isEmpty() && !line.startsWith("//")) {
+                            listaParole.add(line.trim().toLowerCase());
+                        }
+                    }
+                }
+            } else {
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        if (!line.trim().isEmpty() && !line.startsWith("//")) {
+                            listaParole.add(line.trim().toLowerCase());
+                        }
+                    }
+                }
+            }
+
+            if (listaParole.isEmpty()) {
+                parole = new String[] { "ciao", "computer", "vim", "neovim", "vscode" };
+                System.out.println("Nessuna parola trovata nel file, uso quelle predefinite.");
+            } else {
+                parole = listaParole.toArray(new String[0]);
+                System.out.println("Caricate " + parole.length + " parole dal file.");
+            }
+
+        } catch (IOException e) {
+            System.out.println("Errore nella lettura del file: " + e.getMessage());
+            parole = new String[] { "ciao", "computer" };
+        }
+    }
+
+    private boolean contieneLettera(char[] array, char lettera) {
+        for (char c : array) {
+            if (c == lettera)
+                return true;
+        }
+        return false;
+    }
+
+    // #endregion
+
+    // #region disegnoOmino
+    private static final String[] disegnoOmino = {
+            "+---+\n" +
+                    "|   |\n" +
+                    "    |\n" +
+                    "    |\n" +
+                    "    |\n" +
+                    "    |\n" +
+                    "=========",
+
+            "+---+\n" +
+                    "|   |\n" +
+                    "O   |\n" +
+                    "    |\n" +
+                    "    |\n" +
+                    "    |\n" +
+                    "=========",
+
+            "+---+\n" +
+                    "|   |\n" +
+                    "O   |\n" +
+                    "|   |\n" +
+                    "    |\n" +
+                    "    |\n" +
+                    "=========",
+
+            "+---+\n" +
+                    "|   |\n" +
+                    "O   |\n" +
+                    "/|   |\n" +
+                    "    |\n" +
+                    "    |\n" +
+                    "=========",
+
+            "+---+\n" +
+                    "|   |\n" +
+                    "O   |\n" +
+                    "/|\\  |\n" +
+                    "    |\n" +
+                    "    |\n" +
+                    "=========",
+
+            "+---+\n" +
+                    "|   |\n" +
+                    "O   |\n" +
+                    "/|\\  |\n" +
+                    "/    |\n" +
+                    "    |\n" +
+                    "=========",
+
+            "+---+\n" +
+                    "|   |\n" +
+                    "O   |\n" +
+                    "/|\\  |\n" +
+                    "/ \\  |\n" +
+                    "    |\n" +
+                    "========="
+    };
+
+    public String getDisegnoOmino() {
+        int indice = 6 - tentativiRimasti;
+        if (indice < 0)
+            indice = 0;
+        if (indice >= disegnoOmino.length)
+            indice = disegnoOmino.length - 1;
+        return disegnoOmino[indice];
+    }
+
+    // #endregion
+
+    // #region Getters
+    public String getParolaDaIndovinare() {
+        return parolaDaIndovinare;
+    }
+
+    public char[] getLettereIndovinate() {
+        return lettereIndovinate;
+    }
+
+    public StringBuilder getLettereErrate() {
+        return lettereErrate;
+    }
+
+    public int getTentativiRimasti() {
+        return tentativiRimasti;
+    }
+
+    public boolean getPartitaFinita() {
+        return partitaFinita;
+    }
+    // #endregion
+}
---
+++ src/src/main/java/main/HangmanApp.java
@@ -0,0 +1,36 @@
+package main;
+
+import java.util.Scanner;
+import game.HangmanLogic;
+
+public class HangmanApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        HangmanLogic hangman = new HangmanLogic();
+
+        while (!hangman.getPartitaFinita()) {
+            System.out.println("Indovina la parola: " + new String(hangman.getLettereIndovinate()));
+
+            if (hangman.getTentativiRimasti() == 6) {
+                System.out.println("Lettere errate: " + hangman.getLettereErrate());
+                System.out.println("Tentativi rimasti: " + hangman.getTentativiRimasti());
+            } else {
+                System.out.println("Lettere errate: " + hangman.getLettereErrate());
+                System.out.println("Tentativi rimasti: " + hangman.getTentativiRimasti());
+                System.out.println(hangman.getDisegnoOmino());
+            }
+
+            System.out.println("Inserisci una lettera: ");
+            char lettera = sc.next().charAt(0);
+            hangman.indovinaLettera(lettera);
+        }
+
+        if (hangman.getTentativiRimasti() == 0)
+            System.out.println("Hai perso! La parola era: " + hangman.getParolaDaIndovinare());
+        else
+            System.out.println("Hai vinto! La parola era: " + hangman.getParolaDaIndovinare());
+
+        sc.close();
+    }
+
+}
